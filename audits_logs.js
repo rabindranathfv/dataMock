d0c21638e45522e407fe4a51106417e7939e5da7
rabindranathfv <rabindranathucv@gmail.com>
2018-12-27 10:56:19 -0300

    fix amount of data

--- audits_logs.js
+++ audits_logs.js
@@ -2,102 +2,122 @@ module.exports = {
     object: [{
         "timestamp": "4:48",
         "entityType": "Rule Chain",
-        "entityName": "Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. ",
-        "status": "success"
+        "entityName": "Integer non velit. Donec diam neque, vestibulum eget, vulputate ut.",
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "22:03",
         "entityType": "Entity View",
         "entityName": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. ",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "3:43",
         "entityType": "Entity View",
         "entityName": "Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "4:21",
         "entityType": "Entity View",
         "entityName": "In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. ",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "8:56",
         "entityType": "Entity View",
-        "entityName": "Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.",
-        "status": "success"
+        "entityName": "Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec.",
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "21:01",
         "entityType": "Entity View",
         "entityName": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. ",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "19:01",
         "entityType": "Rule Chain",
         "entityName": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "3:41",
         "entityType": "Entity View",
         "entityName": "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "6:00",
         "entityType": "Rule Chain",
         "entityName": "Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "20:08",
         "entityType": "Entity View",
-        "entityName": "Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. ",
-        "status": "success"
+        "entityName": "Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper",
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "12:01",
         "entityType": "Rule Chain",
         "entityName": "Aliquam sit amet diam in magna bibendum imperdiet. ",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "4:23",
         "entityType": "Entity View",
         "entityName": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. ",
-        "status": "cancel"
+        "status": "cancel",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "3:54",
         "entityType": "Entity View",
         "entityName": "Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim.",
-        "status": "cancel"
+        "status": "cancel",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "2:15",
         "entityType": "Rule Chain",
-        "entityName": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo.",
-        "status": "success"
+        "entityName": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis",
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "22:23",
         "entityType": "Entity View",
-        "entityName": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. ",
-        "status": "success"
+        "entityName": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae",
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "22:20",
         "entityType": "Entity View",
         "entityName": "Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat.",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "8:45",
         "entityType": "Entity View",
         "entityName": "Ut at dolor quis odio consequat varius. Integer ac leo.",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "6:36",
         "entityType": "Rule Chain",
         "entityName": "Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis.",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "13:11",
         "entityType": "Entity View",
         "entityName": "Praesent id massa id nisl venenatis lacinia.",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }, {
         "timestamp": "8:59",
         "entityType": "Entity View",
         "entityName": "Maecenas ut massa quis augue luctus tincidunt.",
-        "status": "success"
+        "status": "success",
+        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis perspiciatis accusamus "
     }]
 }
