5a8fb5e5868228d83e6f40c7e1cf1be593a9ec5e
rabindranathfv <rabindranathucv@gmail.com>
2019-01-03 15:27:01 -0300

    refactor home

--- home.js
+++ home.js
@@ -1,41 +1,41 @@
 module.exports = {
     object: [{
-            'moduleId': 1,
+            'id': 1,
             'moduleName': 'Rule Management',
             'icon': 'fa fa-code'
         },
         {
-            'moduleId': 2,
+            'id': 2,
             'moduleName': 'Customer Management',
             'icon': 'fa fa-users'
         },
         {
-            'moduleId': 3,
+            'id': 3,
             'moduleName': 'Asset Management',
             'icon': 'fa fa-th'
         },
         {
-            'moduleId': 4,
+            'id': 4,
             'moduleName': 'Device Management',
             'icon': 'fa fa-tablet'
         },
         {
-            'moduleId': 5,
+            'id': 5,
             'moduleName': 'Entity View Management',
             'icon': 'fa fa-th-large'
         },
         {
-            'moduleId': 6,
+            'id': 6,
             'moduleName': 'Widgets View Management',
             'icon': 'fa fa-tasks'
         },
         {
-            'moduleId': 7,
+            'id': 7,
             'moduleName': 'Dashboard Management',
             'icon': 'icon-speedometer'
         },
         {
-            'moduleId': 8,
+            'id': 8,
             'moduleName': 'Audit',
             'icon': 'fa fa-files-o'
         }
