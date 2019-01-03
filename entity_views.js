6fc09a85ea1245aa0578dc9576df7457986e31c3
rabindranathfv <rabindranathucv@gmail.com>
2019-01-03 15:16:58 -0300

    refactor data

--- entity_views.js
+++ entity_views.js
@@ -1,23 +1,48 @@
 module.exports = {
     object: [{
-        "entityId": 1,
-        "mode": "Ent 1",
-        "description": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue."
-    }, {
-        "entityId": 2,
-        "mode": "Ent 1",
-        "description": "Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula."
-    }, {
-        "entityId": 3,
-        "mode": "Ent 3",
-        "description": "Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante."
-    }, {
-        "entityId": 4,
-        "mode": "Ent 2",
-        "description": "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat."
-    }, {
-        "entityId": 5,
-        "mode": "Ent 4",
-        "description": "Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti."
-    }]
+            "id": 1,
+            "mode": "Ent 1",
+            "name": 'Entity1',
+            "typeData": 'ent',
+            "description": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue."
+        }, {
+            "id": 2,
+            "mode": "Ent 1",
+            "name": 'Entity2',
+            "typeData": 'ent',
+            "description": "Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula."
+        }, {
+            "id": 3,
+            "mode": "Ent 3",
+            "name": 'Entity3',
+            "typeData": 'ent',
+            "description": "Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante."
+        }, {
+            "id": 4,
+            "mode": "Ent 4",
+            "name": 'Entity4',
+            "typeData": 'ent',
+            "description": "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat."
+        }, {
+            "id": 5,
+            "mode": "Ent 5",
+            "name": 'Entity5',
+            "typeData": 'ent',
+            "description": "Duis ac nibh. Fusce , feugiat non, pretium quis, lectus. Suspendisse potenti."
+        },
+        {
+            "id": 4,
+            "mode": "Ent 4",
+            "name": 'Entity4',
+            "typeData": 'ent',
+            "description": "Duis  1234 51231 ac nibh. aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti."
+        },
+        {
+            "id": 5,
+            "mode": "Ent 5",
+            "name": 'Entity5',
+            "typeData": 'ent',
+            "description": "Duis zxñcjlkñas 32184965rus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti."
+        }
+    ]
 }
